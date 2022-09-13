7a12899f0d8144d45c40e6d72f8a293bff4c5264
Santosh2740 <santoshvishwa1572@gmail.com>
2022-09-13 18:06:08 +0530

    LICIOUS   U P D A T E

--- silly-authority-4051-fw19_0794_day-5/component/footer.js
+++
@@ -1,9 +0,0 @@
-import footerTal from "../../../Components/footer.js";
-
-document.getElementById("footer").innerHTML = footerTal();
-
-
-
-import navbar from "../../../Components/navbar.js";
-
-document.getElementById("navbar").innerHTML = navbar();
--- silly-authority-4051-fw19_0794_day-5/components/comp.js
+++ silly-authority-4051-fw19_0794_day-5/components/comp.js
@@ -1,3 +1,8 @@
 import navbar from "../../Components/navbar.js";
-console.log(navbar());
+// console.log(navbar());
 document.getElementById("navbar").innerHTML = navbar();
+
+
+import footerTal from "../../Components/footer.js";
+
+document.getElementById("footer").innerHTML = footerTal();
