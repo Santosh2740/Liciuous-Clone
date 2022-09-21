c535be0d274e30e36d737c3c519179d2fb8e5eb1
Santosh2740 <santoshvishwa1572@gmail.com>
2022-09-21 20:34:26 +0530

    SEARCH FUNCTIONALITY UPDATED

--- ProductDetails/components/satosh_com.js
+++ ProductDetails/components/satosh_com.js
@@ -1,32 +1,27 @@
 import navbar from "../../Components/navbar.js";
 document.getElementById("navbar").innerHTML = navbar();
-// console.log(navbar());
-// navbar();
 
 import footerTal from "../../Components/footer.js";
 document.getElementById("footer").innerHTML = footerTal();
 
-// import searchfun from "../../Components/seachFun.js";
-// searchfun();
+let searchquery = document.querySelector("#search_bar");
 
-// let searchquery = document.querySelector("#search_bar");
+searchquery.addEventListener("keypress", () => {
+  if (event.key == "Enter") {
+    pressing();
+  }
+});
 
-// searchquery.addEventListener("keypress", () => {
-//   if (event.key == "Enter") {
-//     pressing();
-//   }
-// });
-
-// let pressing = async () => {
-//   let query = document.querySelector("#search_bar").value;
-//   query.toLowerCase();
-//   let res = await fetch(
-//     `https://safe-eyrie-39519.herokuapp.com/licious/${query}`
-//   );
-//   let data = await res.json();
-//   console.log(data);
-//   localStorage.setItem("searchresult", JSON.stringify(data));
-//   console.log(query);
-//   window.location.href =
-//     "../../HomePage/searchdata.html";
-// };
+let pressing = async () => {
+  let query = document.querySelector("#search_bar").value;
+  query.toLowerCase();
+  let res = await fetch(
+    `https://safe-eyrie-39519.herokuapp.com/licious/${query}`
+  );
+  let data = await res.json();
+  console.log(data);
+  localStorage.setItem("searchresult", JSON.stringify(data));
+  console.log(query);
+  window.location.href =
+    "../../HomePage/searchdata.html";
+};
